0e862c40c45e122562a6e2f78551d940683baf59
shivangi.kanodia <shivangi.kanodia@BDC4A-D-74MSX52.dir.svc.accenture.com>
2016-06-22 19:29:17 +0530

    initial check in

---
+++ helloworld/src/helloworld/hel.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class hel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("hell");
+   System.out.println("hell");
+   System.out.println("hell");
+	}
+
+}
